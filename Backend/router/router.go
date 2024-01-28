3f24d4dcb342d4e7f805c354dc06028bf0d1b0b7
Amirerfan Teimoori <tamirerfan@gmail.com>
2024-01-28 04:40:15 +0330

    Add CORS middleware

--- Backend/router/router.go
+++ Backend/router/router.go
@@ -9,6 +9,11 @@ import (
 func New() *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.PATCH},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 	userGroup := e.Group("/user")
 	userGroup.POST("/register", handlers.RegisterUser)
 	userGroup.POST("/login", handlers.Login)
