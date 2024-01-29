d4da14aca83a0133ce602969e0ce4f8d61fffbcd
Amirerfan Teimoori <tamirerfan@gmail.com>
2024-01-29 21:57:42 +0330

    Fix endpoint url change

--- Frontend/src/components/LoginForm.tsx
+++ Frontend/src/components/LoginForm.tsx
@@ -17,7 +17,7 @@ const LoginForm = () => {
         setErrorMessage('');
 
         try {
-            const response = await fetch('http://127.0.0.1:8020/user/login', {
+            const response = await fetch('http://127.0.0.1:8020/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
